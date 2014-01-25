108855da70656c6188cd287b5671454ebdb52d4d
michael.schramm <michael.schramm@gmail.com>
2014-01-25 20:18:12 +0100

    minimal configuration

---
+++ src/ms07/SubversionBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ms07\SubversionBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction(){
+        return array();
+    }
+}
---
+++ src/ms07/SubversionBundle/SubversionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ms07\SubversionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SubversionBundle extends Bundle
+{
+}
